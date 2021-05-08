480e141313575e55bb8181162a70db2f51d3232e
liqingwei <lqw332664203@mail.ustc.edu.cn>
2021-05-08 23:49:11 +0800

    change the match structure

--- lab2/src/main.rs
+++ lab2/src/main.rs
@@ -14,25 +14,79 @@ fn main() -> Result<(), Box<dyn Error>> {
 
         let mut line = String::new();
         stdin().read_line(&mut line)?;
-        if line == "" {     //  "Ctrl D" makes the line a empty string
-            println!();     //  print new line to have a better experience and eliminate bad prompts
+        if line == "" {
+            //  "Ctrl D" makes the line a empty string
+            println!(); //  print new line to have a better experience and eliminate bad prompts
             return Ok(());
         }
-        let mut cmds = line.trim().split(" | ").peekable(); //  pipes split commands
+        let mut cmds = line.trim().split("|").peekable(); //  pipes split commands
         let mut previous_command = None;
 
         while let Some(cmd) = cmds.next() {
-            let mut args = cmd.trim().split_ascii_whitespace(); //  in cmd split with whitespace
+            let mut out_redirect: bool = false;
+            let mut out_append_redirect: bool = false;
+            let mut in_redirect: bool = false;
+            if cmd.contains(">") {
+                out_redirect = true;
+            } else if cmd.contains(">>") {
+                out_append_redirect = true;
+            } else if cmd.contains("<") {
+                in_redirect = true;
+            }
+            let mut args = cmd.trim().split_whitespace().peekable(); //  in cmd split with whitespace
             let cmd = match args.next() {
-                None => {                       //  nothing happened in the command
+                None => {
+                    //  nothing happened in the command
                     continue;
                 }
                 Some(cmd) => cmd,
             };
             match cmd {
-                "exit" => {     //  same as "Ctrl D"
+                "exit" => {
+                    //  same as "Ctrl D"
                     println!();
                     return Ok(());
+                }
+                "cd" => {
+                    let dir = match args.next() {
+                        None => match home_path() {
+                            //  "cd\n"
+                            Err(_e) => {
+                                continue;
+                            }
+                            Ok(dir) => dir,
+                        },
+                        Some(dist) => {
+                            //  "cd /path"
+                            if dist == "~" {
+                                //  "cd ~"
+                                match home_path() {
+                                    Err(_e) => {
+                                        continue;
+                                    }
+                                    Ok(dir) => dir,
+                                }
+                            } else {
+                                dist.to_string()
+                            }
+                        }
+                    };
+                    match env::set_current_dir(dir) {
+                        //  change the directory as dir defined
+                        Err(message) => {
+                            println!("{}", message);
+                        }
+                        _ => (),
+                    }
+                }
+                "export" => {
+                    //  set env variable
+                    for arg in args {
+                        let mut assign = arg.split('=');
+                        let name = assign.next().expect("No variable name");
+                        let value = assign.next().expect("No variable value");
+                        env::set_var(name, value);
+                    }
                 }
                 _ => {
                     //  if there is former program has output, then use its stdout as future stdin, else inherit the shell's stdin as future stdin
@@ -49,70 +103,19 @@ fn main() -> Result<(), Box<dyn Error>> {
 
                     //  previous_command has been moved in "let stdin = ..."
                     previous_command = None;
-                    match cmd {
-                        "cd" => {
-                            let dir = match args.next() {
-                                None => match home_path() { //  "cd\n"
-                                    Err(_e) => {
-                                        previous_command = None;
-                                        continue;
-                                    }
-                                    Ok(dir) => dir,
-                                },
-                                Some(dist) => {             //  "cd /path"
-                                    if dist == "~" {        //  "cd ~"
-                                        match home_path() {
-                                            Err(_e) => {
-                                                previous_command = None;
-                                                continue;
-                                            }
-                                            Ok(dir) => dir,
-                                        }
-                                    } else {
-                                        dist.to_string()
-                                    }
-                                }
-                            };
-                            match env::set_current_dir(dir) {   //  change the directory as dir defined
-                                Err(message) => {
-                                    println!("{}", message);
-                                }
-                                _ => (),
-                            }
+                    let output = Command::new(cmd)
+                        .args(args)
+                        .stdin(stdin)
+                        .stdout(stdout)
+                        .spawn(); //  as a child process
+                    match output {
+                        Ok(out) => {
+                            previous_command = Some(out);
                         }
-                        // "pwd" => {
-                        //     let err = "Getting current dir failed";
-                        //     println!("{}", env::current_dir().expect(err).to_str().expect(err));
-                        // }
-                        "export" => {   //  set env variable
-                            for arg in args {
-                                let mut assign = arg.split('=');
-                                let name = assign.next().expect("No variable name");
-                                let value = assign.next().expect("No variable value");
-                                env::set_var(name, value);
-                            }
+                        Err(e) => {
+                            eprintln!("{}", e); //  print error message but not panic
                         }
-                        // "env" => {
-                        //     for (key, value) in env::vars() {
-                        //         println!("{}={}", key, value);
-                        //     }
-                        // }
-                        _ => {
-                            let output = Command::new(cmd)
-                                .args(args)
-                                .stdin(stdin)
-                                .stdout(stdout)
-                                .spawn();   //  as a child process
-                            match output {
-                                Ok(out) => {
-                                    previous_command = Some(out);
-                                }
-                                Err(e) => {
-                                    eprintln!("{}", e); //  print error message but not panic
-                                }
-                            }
-                        }
-                    };
+                    }
                 }
             };
         }
@@ -134,13 +137,20 @@ fn home_path() -> Result<String, VarError> {
 }
 
 fn prompt() -> std::io::Result<()> {
-    print!(
-        "{}: {}$ ",
-        whoami::realname(),
+    println!(
+        "{}: {}",
+        whoami::username(),
         env::current_dir()?
             .to_str()
-            .expect("Getting current dir failed")
+            .expect("Getting current dir failed"),
     );
-    io::stdout().flush().unwrap();
+    print!(
+        "{} ",
+        match whoami::username().as_str() {
+            "root" => "#",
+            _ => "$",
+        }
+    );
+    stdout().flush().unwrap();
     Ok(())
 }
